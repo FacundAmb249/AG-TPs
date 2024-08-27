3466d7346f11131975e1aab6d7439b1a21385f41
manuvariego <73005152+manuvariego@users.noreply.github.com>
2024-08-27 15:30:05 -0300

    change: algunos variables

--- tp2.py
+++ tp2.py
@@ -1,65 +1,72 @@
 import pandas #para armar la tabla de datos del final
 import sys
 
-#Cantidad de elementos para $ máximo para un volumen maxLista
+#matrizObjetos[0] -> Numero objeto
+#matrizObjetos[1] -> Volumen o Peso objeto
+#matrizObjetos[2] -> Valor monetario objeto
 
 #Ordena de mayor a menor
-def ordenaDesc(lista):
-  for i in range(len(lista[0])):
-    for j in range(i+1, len(lista[0])):
-      if (lista[2][i] / lista[1][i]) < (lista[2][j] / lista[1][j]):
+def ordenaDesc(matrizObjetos, cantObjetos):
+  for i in range(cantObjetos):
+    for j in range(i+1, cantObjetos):
+      if (matrizObjetos[2][i] / matrizObjetos[1][i]) < (matrizObjetos[2][j] / matrizObjetos[1][j]):
         #invierte la posición de ambos valores
-        lista[0][i], lista[0][j] = lista[0][j], lista[0][i]
-        lista[1][i], lista[1][j] = lista[1][j], lista[1][i]
-        lista[2][i], lista[2][j] = lista[2][j], lista[2][i]
+        matrizObjetos[0][i], matrizObjetos[0][j] = matrizObjetos[0][j], matrizObjetos[0][i]
+        matrizObjetos[1][i], matrizObjetos[1][j] = matrizObjetos[1][j], matrizObjetos[1][i]
+        matrizObjetos[2][i], matrizObjetos[2][j] = matrizObjetos[2][j], matrizObjetos[2][i]
 
-def greedyBusqueda(lista, maxLista):
-  vpMochila = 0     #cm³ or grs.
-  valorMochila = 0  # $
-  ordenaDesc(lista)
-  for i in range(len(lista[0])):
-    if vpMochila + lista[1][i] <= maxLista:
-      vpMochila += lista[1][i]
-      valorMochila += lista[2][i]
-      elementosEnMochila.append([lista[0][i], lista[1][i], lista[2][i]])
-  return elementosEnMochila, vpMochila, valorMochila
+def greedyBusqueda(matrizObjetos, maxVolumenPeso, cantObjetos):
+  mayorValor = 0 
+  mayorPesoVolumen = 0
+  objetosEnMochila = [] 
+  ordenaDesc(matrizObjetos, cantObjetos)
+  for i in range(cantObjetos):
+    if mayorPesoVolumen + matrizObjetos[1][i] <= maxVolumenPeso:
+      mayorPesoVolumen  += matrizObjetos[1][i]
+      mayorValor += matrizObjetos[2][i]
+      objetosEnMochila.append([matrizObjetos[0][i], matrizObjetos[1][i], matrizObjetos[2][i]])
+  return objetosEnMochila, mayorPesoVolumen, mayorValor
 
-def exhaustivaBusqueda(lista,maxLista): 
-    lista_inicial = [-1] * len(lista[0])
-    algoritmo_backtracking(lista_inicial,maxLista, 0)
-    return valorCombinacion(lista,maxLista)
-
-#Recorre el subConjunto de combinaciones, encuentra el que tiene mayor valor y cumple con la restriccion propuesta
-def valorCombinacion(lista, maxLista):
-  mayValor = 0 
-  totPesoVol = 0
-  for i in range (0, len(subConjunto)):
-    valorMochila = 0 
-    vpMochila = 0
-    LocElemMochila = []
-    for j in range (0, len(lista[0])):
-      if subConjunto[i][j] == 1:
-        vpMochila += lista[1][j]
-        valorMochila += lista[2][j]
-        LocElemMochila.append([lista[0][j], lista[1][j], lista[2][j]])
-    if (vpMochila <= maxLista):
-      if (valorMochila > mayValor):
-        mayValor = valorMochila
-        totPesoVol = vpMochila
-        elementosEnMochila = LocElemMochila[:]
-  return elementosEnMochila, totPesoVol, mayValor
+def exhaustivaBusqueda(matrizObjetos,maxVolumenPeso, cantObjetos): 
+    combinacionObjetos = [-1] * cantObjetos
+    algoritmo_backtracking(combinacionObjetos,maxVolumenPeso, 0, cantObjetos)
+    return valorCombinacion(matrizObjetos,maxVolumenPeso, cantObjetos)
 
 #Genera todas las combinaciones posibles de elementos, mediante recursividad, las guarda en un lista 'subConjunto'
-def algoritmo_backtracking(lista_inicial, maxLista, i):
-    if i == len(lista_inicial):
-        subConjunto.append(lista_inicial[:])
+def algoritmo_backtracking(combinacionObjetos, maxVolumenPeso, i, cantObjetos):
+    if i == cantObjetos:
+        subConjunto.append(combinacionObjetos[:])
         return
 
-    lista_inicial[i] = 0
-    algoritmo_backtracking(lista_inicial,maxLista, i + 1)
+    combinacionObjetos[i] = 0
+    algoritmo_backtracking(combinacionObjetos,maxVolumenPeso, i + 1, cantObjetos)
 
-    lista_inicial[i] = 1
-    algoritmo_backtracking(lista_inicial,maxLista, i + 1)
+    combinacionObjetos[i] = 1
+    algoritmo_backtracking(combinacionObjetos,maxVolumenPeso, i + 1, cantObjetos)
+
+#Recorre el subConjunto de combinaciones, encuentra el que tiene mayor valor y cumple con la restriccion propuesta
+def valorCombinacion(matrizObjetos, maxVolumenPeso, cantObjetos):
+  mayorPesoVolumen = 0
+  mayorValor = 0
+  objetosEnMochila = []
+
+  for i in range (0, len(subConjunto)):
+    locValorMochila = 0 
+    locVolumenPesoMochila = 0
+    locObjetosMochila = []
+    for j in range (0, cantObjetos):
+      if subConjunto[i][j] == 1:
+        locVolumenPesoMochila += matrizObjetos[1][j]
+        locValorMochila += matrizObjetos[2][j]
+        locObjetosMochila.append([matrizObjetos[0][j], matrizObjetos[1][j], matrizObjetos[2][j]])
+
+    if (locVolumenPesoMochila <= maxVolumenPeso):
+      if (locValorMochila > mayorValor):
+        mayorValor = locValorMochila
+        mayorPesoVolumen = locVolumenPesoMochila
+        objetosEnMochila = locObjetosMochila[:]
+  return objetosEnMochila, mayorPesoVolumen, mayorValor
+
 
 # Main
 if len(sys.argv) != 5 or sys.argv[1] != "-b" or sys.argv[3] != "-l":
@@ -75,34 +82,37 @@ elif sys.argv[2] != '1' and sys.argv[2] != '2':
   sys.exit(1)
 elif sys.argv[4] != '1' and sys.argv[4] != '2':
   print("Lista inexistente")
+  sys.exit(1)
 
-#Se podría pasar a una colección de datos para tener una sola variable de lista.
+
+#Inicialización de variables
 lista_1 = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [150, 325, 600, 805, 430, 1200, 770, 60, 930, 353], [20, 40, 50, 36, 25, 64, 54, 18, 46, 28]] #pos, cm³, $
-maxLista_1 = 4200 #cm³
+maxVolumenPeso_1 = 4200 #cm³
 lista_2 = [[1, 2, 3], [1800, 600, 1200], [72, 36, 60]] #pos, grs., $
-maxLista_2 = 3000 #grs.
+maxVolumenPeso_2 = 3000 #grs.
 subConjunto = []
 
 if sys.argv[4] == '1':
-  lista = lista_1
-  maxLista= maxLista_1
+  matrizObjetos = lista_1
+  maxVolumenPeso= maxVolumenPeso_1
+  cantObjetos = len(lista_1[0])
   columnas = ["N. objeto", "Volúmen (cm³)", "Valor ($)"]
   unidad = "cm³"
 elif sys.argv[4] == '2':
-  lista = lista_2
-  maxLista= maxLista_2
+  matrizObjetos = lista_2
+  maxVolumenPeso= maxVolumenPeso_2
+  cantObjetos = len(lista_2[0])
   columnas = ["N. objeto", "Peso (grs.)", "Valor ($)"]
   unidad = "grs."
 
-elementosEnMochila = []
 
 if sys.argv[2] == '1': #Búsqueda exhaustiva
-  elementosEnMochila, vpMochila, valorMochila = exhaustivaBusqueda(lista, maxLista)
+  objetosEnMochila, mayorPesoVolumen, mayorValor = exhaustivaBusqueda(matrizObjetos, maxVolumenPeso, cantObjetos)
 elif sys.argv[2] == '2': #Búsqueda greedy (golosa)
-  elementosEnMochila, vpMochila, valorMochila = greedyBusqueda(lista, maxLista)
+  objetosEnMochila, mayorPesoVolumen, mayorValor = greedyBusqueda(matrizObjetos, maxVolumenPeso, cantObjetos)
 
-datos = pandas.DataFrame(elementosEnMochila, columns=columnas)
+datos = pandas.DataFrame(objetosEnMochila, columns=columnas)
 print(datos)
-print("Capacidad total =", maxLista, unidad)
-print("Peso total =", vpMochila, unidad)
-print("Precio final = $", valorMochila)
+print("Capacidad total =", maxVolumenPeso, unidad)
+print("Peso total =", mayorPesoVolumen , unidad)
+print("Precio final = $", mayorValor)
